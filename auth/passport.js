e5d696b2954974fe8a27abbed27e1b07da1fe664
virtuallyawake <virtuallyawake@gmail.com>
2014-05-30 20:18:47 +0200

    Removed hard-coded users and added authentication against the users in mongodb.

--- auth/passport.js
+++ auth/passport.js
@@ -1,50 +1,17 @@
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var hash = require('../helpers/hash');
 
 module.exports = function(passport) {
-    // List of authorized users
-    var users = [
-	{
-	    id: 1,
-	    username: 'john', 
-	    password: '1234', 
-	    email: 'john@doe.com' 
-	},
-	{ 
-	    id: 2,
-	    username: 'jane',
-	    password: '5678',
-	    email: 'jane@doe.com'
-	}
-    ];
-
-    function findById(id, fn) {
-	var idx = id - 1;
-	if (users[idx]) {
-	    fn(null, users[idx]);
-	} else {
-	    fn(new Error('User ' + id + ' does not exist'));
-	}
-    }
-
-    function findByUsername(username, fn) {
-	for (var i = 0, len = users.length; i < len; i++) {
-	    var user = users[i];
-	    if (user.username === username) {
-		return fn(null, user);
-	    }
-	}
-	return fn(null, null);
-    }
 
     // Passport session setup
     passport.serializeUser(function(user, done) {
-	done(null, {id: user.id, username: user.username});
+	done(null, {username: user._id});
     });
 
     passport.deserializeUser(function(userData, done) {
-	findById(userData.id, function (err, user) {
+	User.findOne(userData.username, function (err, user) {
 	    done(err, user);
 	});
     });
@@ -58,14 +25,14 @@ module.exports = function(passport) {
 		// username, or the password is not correct, set the user to `false` to
 		// indicate failure and set a flash message.  Otherwise, return the
 		// authenticated `user`.
-		findByUsername(username, function(err, user) {
+		User.findOne({_id: username}, function(err, user) {
 		    if (err)
 			return done(err);
  
 		    if (!user) { 
 			return done(null, false, { message: 'Unknown user ' + username });
 		    }
-		    if (user.password != password) {
+		    if (user.hash !== hash(password, user.salt)) {
 			return done(null, false, { message: 'Invalid password' });
 		    }
 		    return done(null, user);
